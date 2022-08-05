893671aa6e3cac70c993b2e381ff0f66c3b62037
juanfonseca09 <110582471+juanfonseca09@users.noreply.github.com>
2022-08-05 09:27:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,15 +19,23 @@ alert ("Que operacion queres realizar? ");
 let operacion = prompt("1: Sumar  2: Restar 3:  Multiplicar 4: Dividir " )
 
 if (operacion == 1) {
-    let numero1 = prompt("Primer numero para sumar ");
-    let numero2 = prompt("segundo  numero para sumar ");
+    let numero1 = prompt("Primer numero para sumar: ");
+    let numero2 = prompt("segundo  numero para sumar: ");
     resultado = sumar (numero1,numero2);
     document.write (`tu resultado es ${resultado}`);
 }
     
 if (operacion == 4) {
-    let numero1 = prompt("Primer número para sumar");
-    let numero2 = prompt("Segundo número para sumar");
+    let numero1 = prompt("Primer número para dividir: ");
+    let numero2 = prompt("Segundo número para dividir: ");
     resultado = dividir (numero1,numero2);
     document.write (`Tu resultado es ${resultado}`);
 }
+if (operacion == 3) {
+    let num1 = prompt("Primer numero para multiplicar: ");
+    let num2 = prompt("Segundo numero para multiplicar: ");
+    resultado = multiplicar (num1,num2);
+    document.write (`tu resultado es ${resultado}`);
+}
+    
+    
