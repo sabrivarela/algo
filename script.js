e63e3c6af9138c9fcdf9eb5c9c965da0fa56c949
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-05 09:13:23 -0300

    Update script.js
    Este cambio es para hacer las sumas

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ const dividr = (num1, num2)=>{
 alert ("Que operacion queres realizar? ");
 
 let operacion = prompt("1: Sumar  2: Restar 3:  Multiplicar 4: Dividir " )
+
+if (operacion == 1) {
+    let numero1 = prompt("Primer numero para sumar ");
+    let numero2 = prompt("segundo  numero para sumar ");
+    resultado = sumar (numero1,numero2);
+    document.write (`tu resultado es ${resultado}`);
