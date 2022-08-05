7ce48a12e6c52593289b0eaa7baa2d47fc776118
sabrivarela <110421435+sabrivarela@users.noreply.github.com>
2022-08-05 09:33:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const sumar = (num1, num2)=>{
-    return parseInt(num1) + parseInt (num2)
+    return parseInt(num1) + parseInt (num2);
 }
 
 const restar = (num1, num2)=>{
-    return parseInt(num1) - parseInt (num2)
+    return parseInt(num1) - parseInt (num2);
 }
 
 const multiplicar = (num1, num2)=>{
